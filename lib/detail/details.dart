11b9406af3523886cb94f662792c1bfe7e23379f
Léo Ségalini <130433746+Lejardinierdu33@users.noreply.github.com>
2023-08-31 12:55:38 +0400

    MAJ

--- lib/detail/details.dart
+++ lib/detail/details.dart
@@ -1,18 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:ice_cream_shop/models/ice_cream.dart';
 
-
 class DetailIceCream extends StatelessWidget {
-  const DetailIceCream(this.cesarean, {super.key});
+  const DetailIceCream(this.cesarean, {Key? key}) : super(key: key);
 
   final IceCream cesarean;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-      title: Text(cesarean.name),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 200, // Ajustez la hauteur en conséquence
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.asset(
+                cesarean.bgImage,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        cesarean.name,
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      // Ajoutez d'autres informations ici
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
